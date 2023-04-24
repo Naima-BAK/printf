c64c1bc4e849439819dd172ca7ed514958c94b2e
Naima-BAK <n.bakenchich@gmail.com>
2023-04-24 14:06:18 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -1,47 +1,61 @@
 #include "main.h"
-#include "functions.h"
+
 /**
- * _printf - produces output according to a format.
- * @format: character string containing directives.
- * Return: number of characters printed (excluding null byte)
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
 
+void print_buffer(char buffer[], int *buff_ind)
+{
+if (*buff_ind > 0)
+write(1, &buffer[0], *buff_ind);
+*buff_ind = 0;
+}
+
+/**
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 int _printf(const char *format, ...)
 {
+int i, printed = 0, count = 0;
+int flags, width, precision, size, buff_ind = 0;
 va_list args;
-int i, count = 0;
+char buffer[BUFF_SIZE];
+
 if (format == NULL)
 return (-1);
 
 va_start(args, format);
 
-for (i = 0; format[i] != '\0'; i++)
+for (i = 0; format && format[i] != '\0'; i++)
 {
-if (format[i] == '%')
+if (format[i] != '%')
 {
-if (format[i + 1] == '\0')
-return (-1);
-i++;
-while (format[i] == ' ')
-i++;
-if (format[i] == '%')
-count += print_percent(args);
-else if (format[i] == 'c')
-count += print_char(args);
-else if (format[i] == 's')
-count += print_string(args);
-else if (format[i] == 'i')
-count += print_int(args);
-else if (format[i] == 'd')
-count += print_int(args);
+buffer[buff_ind++] = format[i];
+if (buff_ind == BUFF_SIZE)
+print_buffer(buffer, &buff_ind);
+count++;
 }
 else
 {
-putchar(format[i]);
-count++;
+print_buffer(buffer, &buff_ind);
+flags = get_flags(format, &i);
+width = get_width(format, &i, args);
+precision = get_precision(format, &i, args);
+size = get_size(format, &i);
+++i;
+printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+if (printed == -1)
+return (-1);
+count += printed;
 }
 }
-
+print_buffer(buffer, &buff_ind);
 va_end(args);
+
 return (count);
 }
--- main.h
+++ main.h
@@ -1,8 +1,53 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+
+#include <stdlib.h>
 #include <stdarg.h>
-int print_string(va_list args);
-int print_char(va_list args);
+#include <stdio.h>
+
+/**
+ * struct flags - struct containing flags to "turn on"
+ * when a flag specifier is passed to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+typedef struct flags
+{
+	int plus;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct printHandler - struct to choose the right function depending
+ * on the format specifier passed to _printf()
+ * @c: format specifier
+ * @f: pointer to the correct printing function
+ */
+typedef struct printHandler
+{
+	char c;
+	int (*f)(va_list ap, flags_t *f);
+} ph;
+
+/* print_nums */
+int print_int(va_list l, flags_t *f);
+
+/* _printf */
 int _printf(const char *format, ...);
+
+/* get_print */
+int (*get_print(char s))(va_list, flags_t *);
+
+/* get_flag */
+int get_flag(char s, flags_t *f);
+
+/* print_alpha */
+int print_string(va_list l, flags_t *f);
+int print_char(va_list l, flags_t *f);
+
+/* print_percent */
+int print_percent(va_list l, flags_t *f);
+
 #endif
