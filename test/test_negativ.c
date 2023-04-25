8e790c12adbb496548620d383f5cb60389f37dfb
Naima-BAK <n.bakenchich@gmail.com>
2023-04-25 16:36:32 -0700

    -

---
+++ test/test_negativ.c
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+ _printf("Negative:[%d]\n", -762534);
+printf("Negative:[%d]\n", -762534);
+
+return (0);
+}
